7d57770d6ccab09181f1b2e5f0f180a3422ef9bf
JMartins <60708319+utikati@users.noreply.github.com>
2022-09-09 14:31:51 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 import AdviceComp from "./components/AdviceComp";
-import ImageSaint from "./components/ImageSaint";
 
 function App() {
   const st = {
